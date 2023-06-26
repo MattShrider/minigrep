ae5148cb6244d4da659082c383f90d386bcbc159
Matthew Shrider <mattshrider@gmail.com>
2023-06-26 00:06:37 -0400

    refactor: Make run_file private
    Also cleans up the minigrep::run function implementation to
    skip an unneccesary match expression.

--- src/lib.rs
+++ src/lib.rs
@@ -17,19 +17,16 @@ pub struct SearchMatch {
 }
 
 pub fn run(files: &[String], search: &String) -> Result<Vec<SearchMatch>, MinigrepError> {
-    files.iter().try_fold(
-        vec![],
-        |mut acc: Vec<SearchMatch>, filename| match run_file(filename, search) {
-            Ok(mut results) => {
-                acc.append(&mut results);
-                Ok(acc)
-            }
-            Err(why) => Err(why),
-        },
-    )
+    files
+        .iter()
+        .try_fold(vec![], |mut acc: Vec<SearchMatch>, filename| {
+            let mut results = search_file(filename, search)?;
+            acc.append(&mut results);
+            Ok(acc)
+        })
 }
 
-fn run_file(filename: &String, search: &String) -> Result<Vec<SearchMatch>, MinigrepError> {
+fn search_file(filename: &String, search: &String) -> Result<Vec<SearchMatch>, MinigrepError> {
     let file = open_file(filename)?;
     let reader = BufReader::new(file);
 
@@ -57,7 +54,7 @@ fn run_file(filename: &String, search: &String) -> Result<Vec<SearchMatch>, Mini
     Ok(found)
 }
 
-pub fn open_file(filename: &String) -> Result<File, MinigrepError> {
+fn open_file(filename: &String) -> Result<File, MinigrepError> {
     File::open(Path::new(filename)).or_else(|error| {
         Err(match error.kind() {
             std::io::ErrorKind::NotFound => MinigrepError::FileNotFound(filename.to_owned()),
